2073052cedc66ea822615cba0f3edb9b78e0ce17
Roberto Larivera <robylariv@gmail.com>
2023-01-11 15:44:59 +0100

    Completato griglia, celle, numeri cor., cella cliccata

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 console.log('int ok');
+
+const containerGrid = document.getElementById('container-grid');
+
+for (let i = 1 ; i <= 100 ; i++){
+    const newCell = document.createElement('div');
+    newCell.classList.add('cell');
+    newCell.innerHTML = i;
+    containerGrid.append(newCell);
+
+    newCell.addEventListener('click',
+        function () {
+            if(newCell.classList.contains('cell-active')){
+                newCell.classList.remove('cell-active');
+                console.log('Hai cliccato su ', i);
+            }
+            else{
+                newCell.classList.add('cell-active');
+                console.log('Hai cliccato su ', i);
+
+            }
+        }
+    )
+}
+
