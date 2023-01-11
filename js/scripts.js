933eefacf56aa02eb3701d6e63ba31c2a9faf64c
Roberto Larivera <robylariv@gmail.com>
2023-01-11 15:53:40 +0100

    creazione button - creazione click in caso di restart

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,63 @@
 console.log('int ok');
 
 const containerGrid = document.getElementById('container-grid');
+const submitStart = document.getElementById('submit-start');
 
-for (let i = 1 ; i <= 100 ; i++){
-    const newCell = document.createElement('div');
-    newCell.classList.add('cell');
-    newCell.innerHTML = i;
-    containerGrid.append(newCell);
+submitStart.addEventListener('click',                   // click su start
 
-    newCell.addEventListener('click',
-        function () {
-            if(newCell.classList.contains('cell-active')){
-                newCell.classList.remove('cell-active');
-                console.log('Hai cliccato su ', i);
-            }
-            else{
-                newCell.classList.add('cell-active');
-                console.log('Hai cliccato su ', i);
-
+    function(){                                         // click su start
+        if(containerGrid == ""){
+            console.log('La griglia è VUOTA')
+            for (let i = 1 ; i <= 100 ; i++){
+                const newCell = document.createElement('div');
+                newCell.classList.add('cell');
+                newCell.innerHTML = i;
+                containerGrid.append(newCell);
+            
+                newCell.addEventListener('click',
+                    function () {
+                        if(newCell.classList.contains('cell-active')){
+                            newCell.classList.remove('cell-active');
+                            console.log('Hai cliccato su ', i);
+                        }
+                        else{
+                            newCell.classList.add('cell-active');
+                            console.log('Hai cliccato su ', i);
+            
+                        }
+                    }
+                )
             }
         }
-    )
-}
+        else{
+            console.log('La griglia è PIENA, verrà svuotata e poi riempita di nuovo')
+            containerGrid.innerHTML = "";
+            for (let i = 1 ; i <= 100 ; i++){
+                const newCell = document.createElement('div');
+                newCell.classList.add('cell');
+                newCell.innerHTML = i;
+                containerGrid.append(newCell);
+            
+                newCell.addEventListener('click',
+                    function () {
+                        if(newCell.classList.contains('cell-active')){
+                            newCell.classList.remove('cell-active');
+                            console.log('Hai cliccato su ', i);
+                        }
+                        else{
+                            newCell.classList.add('cell-active');
+                            console.log('Hai cliccato su ', i);
+            
+                        }
+                    }
+                )
+            }
+        }
+
+        
+
+    }                                                   // fine click su start
+
+)                                                       // fine click su start
+
 
