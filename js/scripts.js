0237fa3d440abd91856e14722debbd8a6f9535b7
Roberto Larivera <robylariv@gmail.com>
2023-01-11 17:03:55 +0100

    completata funzione principale

--- js/scripts.js
+++ js/scripts.js
@@ -13,56 +13,64 @@ submitStart.addEventListener('click',                   // click su start
         
         if(selectionDif == 'easy'){
             console.log('Hai inserito EASY');
+            // funzione principale
+        createCellGrid(containerGrid,100);                  //chiamata funzione principale
+        
         }
         else if(selectionDif == 'medium'){
             console.log('Hai inserito MEDIUM');
+            // funzione principale
+        createCellGrid(containerGrid,81);                  //chiamata funzione principale
+        
         }
         else if(selectionDif == 'hard'){
             console.log('Hai inserito HARD');
+            // funzione principale
+        createCellGrid(containerGrid,49);                  //chiamata funzione principale
+        
         }
         else{
             alert('Seleziona una difficoltà')
         }
 
 
-        // funzione principale
-        const newCell = createCellGrid(containerGrid);
         
 
     }                                                   // fine click su start
 )                                                       // fine click su start
 
 
-function createCellGrid(containerGrid){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createCellGrid(containerGrid,n){                 //funzione principale ---- creazione celle dentro griglia 
     containerGrid.innerHTML = "";                       //serve per svuotare il div prima di doverlo riempire
-    for (let i = 1 ; i <= 100 ; i++){
+    for (let i = 1 ; i <= n ; i++){
         
         const newCell = document.createElement('div');
         newCell.classList.add('cell');
         newCell.innerHTML = i;
         containerGrid.append(newCell);
 
-
-        const controlCellActive = controlCellActive (newCell);
-        /*
-        newCell.addEventListener('click',
-                function () {
-                    if(newCell.classList.contains('cell-active')){
-                        newCell.classList.remove('cell-active');
-                        console.log('Hai cliccato su ', i);
-                    }
-                    else{
-                        newCell.classList.add('cell-active');
-                        console.log('Hai cliccato su ', i);
-        
-                    }
-                }
-            )*/
+        controlCellActive (newCell, i);                   //chiamata controllo click per cella attiva
 
     }
 }
 
-function controlCellActive (newCell){                          //controllo click per cella attiva
+function controlCellActive (newCell, i){                          //controllo click per cella attiva
     newCell.addEventListener('click',
                 function () {
                     if(newCell.classList.contains('cell-active')){
